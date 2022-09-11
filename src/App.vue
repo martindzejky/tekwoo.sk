55c089b1f657e7ac7c3c9497d1b68308a193208e
Martin Jakubik <martin@jakubik.info>
2022-09-11 16:13:14 +0200

    extract Navigation.vue

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,7 @@
 <template>
-    <Transition
-        appear
-        appear-active-class="transition duration-1000 ease-out delay-500"
-        appear-from-class="-translate-y-full"
-    >
-        <div
-            class="fixed font-handwritten inset-x-0 flex gap-4 items-end text-white z-10 py-4 px-10"
-        >
-            <RouterLink class="text-4xl hover:text-light transition cursor-pointer" to="/">
-                Tekwoo
-            </RouterLink>
-
-            <div class="grow"></div>
-
-            <a
-                class="text-lg leading-snug hover:text-light transition cursor-pointer"
-                href="mailto:martindzejky@gmail.com"
-            >
-                Kontakt
-            </a>
-
-            <RouterLink
-                class="text-lg leading-snug hover:text-light transition cursor-pointer"
-                to="/7"
-            >
-                Dielňa
-            </RouterLink>
-        </div>
-    </Transition>
-
+    <Navigation />
     <RouterView />
 </template>
+<script setup lang="ts">
+import Navigation from '@/components/Navigation.vue';
+</script>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,34 @@
+<template>
+    <Transition
+        appear
+        appear-active-class="transition duration-1000 ease-out delay-500"
+        appear-from-class="-translate-y-full"
+    >
+        <div
+            class="fixed font-handwritten inset-x-0 flex gap-4 items-end text-white z-10 py-4 px-10"
+        >
+            <RouterLink
+                class="text-4xl hover:text-light hover:scale-110 transition cursor-pointer"
+                to="/"
+            >
+                Tekwoo
+            </RouterLink>
+
+            <div class="grow"></div>
+
+            <a
+                class="text-lg leading-snug hover:text-light hover:scale-110 transition cursor-pointer"
+                href="mailto:martindzejky@gmail.com"
+            >
+                Kontakt
+            </a>
+
+            <RouterLink
+                class="text-lg leading-snug hover:text-light hover:scale-110 transition cursor-pointer"
+                to="/7"
+            >
+                Dielňa
+            </RouterLink>
+        </div>
+    </Transition>
+</template>
