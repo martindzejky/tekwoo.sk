795ae00ca7312a761724938e6c29cd9e6cf5f49b
Martin Jakubik <martin@jakubik.info>
2022-09-11 15:32:43 +0200

    preload the home bg image

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,22 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import Home from './views/Home.vue';
 
+import HOME_IMG from '@/assets/images/home.jpg';
+import { preloadImage } from '@/preload';
+
+declare module 'vue-router' {
+    interface RouteMeta {
+        // if defined, these image URLs are preloaded before navigating to the route
+        preloadImages?: Array<string>;
+    }
+}
+
 const routes: Array<RouteRecordRaw> = [
     {
         path: '/',
         name: 'home',
         component: Home,
+        meta: { preloadImages: [HOME_IMG] },
     },
     {
         path: '/7',
@@ -17,6 +28,16 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes,
+
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) return savedPosition;
+        return { top: 0 };
+    },
+});
+
+router.beforeEach(async (to) => {
+    if (!to.meta?.preloadImages) return;
+    await Promise.all(to.meta.preloadImages.map((src) => preloadImage(src)));
 });
 
 export default router;
